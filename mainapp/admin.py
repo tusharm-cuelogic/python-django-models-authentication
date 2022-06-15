3a958b2cef70af4388c8cb73c982515dd3ca7814
Santosh Jagatap <santosh.jagtap@lntinfotech.com>
2022-06-15 20:02:12 +0530

    Completed module

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import BlogPost
+from .models import Tag
 
 
 admin.site.register(BlogPost)
+admin.site.register(Tag)
--- mainapp/views.py
+++ mainapp/views.py
@@ -31,3 +31,10 @@ def index(request):
 def post(request, id):
     post = get_object_or_404(BlogPost, pk=id)
     return render(request, 'mainapp/post.html', {'object': post})
+
+def tag_posts(request, name):
+    name = name.lower()
+    title = "Posts about {}".format(name)
+    tag = get_object_or_404(Tag, name=name)
+    posts = BlogPost.objects.filter(tags=tag)
+    render(request, 'mainapp/filtered_post_list.html', {'title': title, 'posts':posts})
